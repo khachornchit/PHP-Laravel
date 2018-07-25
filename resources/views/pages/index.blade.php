3a43b6c0ecdc8d93b7fa5d000a5283a0384e4af3
Kajornjit <john@iglu.net>
2018-07-25 13:34:07 +0700

    Initialization

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{config('app_locale')}}">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>{{config('app.name')}}</title>
+</head>
+<body>
+<h1>Welcome to Pluto Solutions</h1>
+<h2>This is the <i>laravel application</i>. It is a part of the PHP framework that we use to develop our applications.</h2>
+</body>
+</html>
